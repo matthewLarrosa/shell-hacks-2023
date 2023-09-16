fa34f272ea5ee6d0b40a47f428fe808e763fb050
Matthew Larrosa <Matthewterra16@gmail.com>
2023-09-16 01:15:43 -0400

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
+import logo from "../assets/stateFarmLogo.png";
+
 export default function Navbar() {
   return (
-    <div className="bg-black">
-      <div className="w-screen h-11">
-        <div className="">Navbar</div>
-        <div>test</div>
+    <div className="flex justify-center">
+      <div className="bg-white border-b border-gray-700 w-9/12 h-11">
+        <div className="flex flex-row">
+          <img className="p-2 mt-2 h-9 md:w-44" src={logo} alt="logo" />
+          <div className="p-2 mt-2 font-zilla">Online Quoting</div>
+        </div>
       </div>
     </div>
   );
