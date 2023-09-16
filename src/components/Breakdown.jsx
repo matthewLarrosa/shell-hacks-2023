e7cfc9fd2d8f4c62547d2db887393e5b4adb4d2a
Matthew Larrosa <Matthewterra16@gmail.com>
2023-09-16 13:31:49 -0400

    info cards

--- src/components/Breakdown.jsx
+++ src/components/Breakdown.jsx
@@ -1,29 +1,40 @@
-import car from '../assets/car.jpg'
-
 export default function Breakdown() {
-
-    const imageUrl= {car}
-    const title="Card Title 2"
-    const description="Description for Card 2."
 
   return (
     <div className="flex w-screen h-auto p-2">
-      <div>
-        <div>Finding a policy that fits your needs is as simple as 1,2,3!</div>
         <div>
-          Looking for auto? No problem. The entire process can be broken n/ into
-          3 steps.
-        </div>
-        <div className="flex justify-center border items-center h-screen">
-          <div className="max-w-xs rounded-custom overflow-hidden shadow-lg">
-            <img className="w-full" src={car} alt={title} />
-            <div className="px-4 py-2 bg-white">
-              <h2 className="font-bold text-lg mb-2">{title}</h2>
-              <p className="text-sm">{description}</p>
+            <div>Finding a policy that fits your needs is as simple as 1,2,3!</div>
+            <div>
+            Looking for auto? No problem. The entire process can be broken n/ into
+            3 steps.
             </div>
-          </div>
+            <div className="py-2">
+                <div className="px-2 mt-2">
+                    <div className="w-auto h-56 outline-black outline-1 border-solid border-2 overflow-hidden rounded-xl shadow-xl bg-[url('https://thumbnails.production.thenounproject.com/nfuzQDyj6LfHqyMrryHXKawd-SU=/fit-in/1000x1000/photos.production.thenounproject.com/photos/74B01E80-C6D4-4E27-838C-197454C7D311.jpg')] bg-cover bg-center">
+                        <div className="w-full h-full flex flex-col justify-center items-center">
+                            <span className="font-zilla font-bold drop-shadow-lg text-white text-xl bottom-0 left-0 text-center">Step 1: Personal Information</span>
+                            <span className="font-zilla font-medium drop-shadow-lg text-white text-lg bottom-0 left-0 text-center">Click to learn more</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="px-2 mt-2">
+                    <div className="w-auto h-56 outline-black outline-1 border-solid border-2 overflow-hidden rounded-xl shadow-xl bg-[url('https://file.kelleybluebookimages.com/kbb/base/house/2022/2022-Toyota-Prius%20Prime-FrontSide_TOPRPME2201_640x480.jpg')] bg-cover bg-center">
+                        <div className="w-full h-full flex flex-col justify-center items-center">
+                            <span className="font-zilla font-bold drop-shadow-xl text-white text-xl bottom-0 left-0 text-center">Step 2: Vehicle Information</span>
+                            <span className="font-zilla font-medium drop-shadow-xl text-white text-lg bottom-0 left-0 text-center">Click to learn more</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="px-2 mt-2">
+                    <div className="w-auto h-56 outline-black outline-1 border-solid border-2 overflow-hidden rounded-xl shadow-xl bg-[url('https://www.bankrate.com/2022/11/09104919/buy-car-or-house-first.jpg?auto=webp&optimize=high&crop=16:9')] bg-cover bg-center">
+                        <div className="w-full h-full flex flex-col justify-center items-center">
+                            <span className="font-zilla font-bold drop-shadow-lg text-white text-xl bottom-0 left-0 text-center">Step 3: Quote Selection</span>
+                            <span className="font-zilla font-medium drop-shadow-lg text-white text-lg bottom-0 left-0 text-center">Click to learn more</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
   );
 }
